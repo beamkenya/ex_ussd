82047bc531692d924af4346baa5cd8ae19b25ffa
Anthony Leiro <lenileiro@gmail.com>
2021-05-24 14:46:21 +0300

    chore: Added phoenix example

---
+++ example/phoenix_app/lib/phoenix_app/ussd/home.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixApp.MyHomeHandler do
+  use ExUssd.Handler
+
+  def init(menu, _api_parameters) do
+    menu
+    |> ExUssd.set(title: "Examples")
+    |> ExUssd.add(ExUssd.new(name: "Simple List", handler: App.SimpleList.MyHomeHandler))
+    |> ExUssd.add(ExUssd.new(name: "Nested List", handler: App.NestedList.MyHomeHandler))
+    |> ExUssd.add(ExUssd.new(name: "Simple Callback", handler: App.SimpleCallback.MyHomeHandler))
+    |> ExUssd.add(ExUssd.new(name: "Nested Callback", handler: App.NestedCallback.PinHandler))
+    |> ExUssd.add(
+      ExUssd.new(name: "Dynamic Vertical menus", handler: App.Dymanic.Vertical.MyHomeHandler)
+    )
+    |> ExUssd.add(
+      ExUssd.new(name: "Dynamic Horizontal menus", handler: App.Dymanic.Horizontal.MyHomeHandler)
+    )
+  end
+end
---
+++ example/phoenix_app/lib/phoenix_app_web/router.ex
@@ -0,0 +1,48 @@
+defmodule PhoenixAppWeb.Router do
+  use PhoenixAppWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, {PhoenixAppWeb.LayoutView, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", PhoenixAppWeb do
+    pipe_through :browser
+
+    live "/", PageLive, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", PhoenixAppWeb do
+  #   pipe_through :api
+  # end
+
+  # Enables LiveDashboard only for development
+  #
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+    import ExUssd
+
+    scope "/" do
+      pipe_through :browser
+      live_dashboard "/dashboard", metrics: PhoenixAppWeb.Telemetry
+
+      simulate "/simulator",
+      menu: ExUssd.new(name: "Home", handler:  PhoenixApp.MyHomeHandler),
+      phone_numbers: ["254700100100", "254700200200", "254700300300"]
+    end
+  end
+end
