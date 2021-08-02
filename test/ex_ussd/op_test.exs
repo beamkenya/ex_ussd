f3cdade69a7b016dae7bbef0b8d9b523a8d1133b
Anthony Leiro <lenileiro@gmail.com>
2021-08-03 00:38:13 +0300

    chore: move module into test file

--- test/ex_ussd/op_test.exs
+++ test/ex_ussd/op_test.exs
@@ -1,3 +1,55 @@
+defmodule ExUssd.OpTest.Module do
+  @moduledoc false
+  def ussd_init(menu, _) do
+    menu
+    |> ExUssd.set(title: "Enter your PIN")
+  end
+
+  def ussd_callback(menu, api_parameters, _) do
+    if api_parameters.text == "5555" do
+      menu
+      |> ExUssd.set(title: "You have Entered the Secret Number, 5555")
+      |> ExUssd.set(should_close: true)
+    end
+  end
+
+  def simple(menu, _) do
+    menu
+    |> ExUssd.set(title: "Welcome")
+    |> ExUssd.add(
+      ExUssd.new(
+        name: "menu 1",
+        resolve: &simple/2
+      )
+      |> ExUssd.set(split: 3)
+    )
+    |> ExUssd.add(
+      ExUssd.new(
+        name: "menu 2",
+        resolve: fn menu, _ -> ExUssd.set(menu, title: "menu 2") end
+      )
+    )
+    |> ExUssd.add(
+      ExUssd.new(
+        name: "menu 3",
+        resolve: fn menu, _ -> ExUssd.set(menu, title: "menu 3") end
+      )
+    )
+    |> ExUssd.add(
+      ExUssd.new(
+        name: "menu 4",
+        resolve: fn menu, _ -> ExUssd.set(menu, title: "menu 4") end
+      )
+    )
+    |> ExUssd.add(
+      ExUssd.new(
+        name: "menu 5",
+        resolve: fn menu, _ -> ExUssd.set(menu, title: "menu 5") end
+      )
+    )
+  end
+end
+
 defmodule ExUssd.OpTest do
   @moduledoc false
   use ExUnit.Case
@@ -91,7 +143,7 @@ defmodule ExUssd.OpTest do
   describe "goto/1 simple" do
     setup do
       %{
-        menu: ExUssd.new(name: Faker.Company.name(), resolve: &ExUssd.Example.simple/2),
+        menu: ExUssd.new(name: Faker.Company.name(), resolve: &ExUssd.OpTest.Module.simple/2),
         session: "#{System.unique_integer()}"
       }
     end
@@ -172,7 +224,7 @@ defmodule ExUssd.OpTest do
   describe "goto/1 with callback" do
     setup do
       %{
-        menu: ExUssd.new(name: Faker.Company.name(), resolve: ExUssd.Example),
+        menu: ExUssd.new(name: Faker.Company.name(), resolve: ExUssd.OpTest.Module),
         session: "#{System.unique_integer()}"
       }
     end
