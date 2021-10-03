0d893b6e9b4de78f2dfe2c5f1b60270d4e7b70ee
Anthony Leiro <lenileiro@gmail.com>
2021-10-03 12:02:24 +0300

    chore: remove unused imports

--- lib/ex_ussd/utils.ex
+++ lib/ex_ussd/utils.ex
@@ -3,7 +3,6 @@ defmodule ExUssd.Utils do
   alias ExUssd.Executer
 
   @default_value 436_739_010_658_356_127_157_159_114_145
-  @is_zero_based 741_463_257_579_241_461_489_157_167_458
 
   @spec to_int(term() | {integer(), String.t()}, ExUssd.t(), map(), String.t()) :: integer()
   def to_int(input, menu, _, input_value)
